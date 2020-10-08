e44c352628c5923c52ff55f9b36c5a72bd0b6bed
JJ <nikoips3@gmail.com>
2020-10-08 16:28:15 +0200

    ajout helloword + conditions python3

---
+++ python/condition.py
@@ -0,0 +1,6 @@
+chiffre = 9
+
+if chiffre % 2 == 0:
+    print("chiffre pair")
+else:
+    print("chiffre impair")
---
+++ python/helloword.py
@@ -0,0 +1,12 @@
+monText = "Bonjour" #Variable de type texte (string)
+chiffre1 = 9        #Variable de tpe entier (int)
+chiffre2 = 4     #Variable de type chiffre à virgule (float)
+flag = True      #Variable de type bollean (bool) 
+
+resultat1 = chiffre1 + chiffre2
+resultat2 = chiffre1 * chiffre2
+resultat3 = chiffre1 // chiffre2 # division entiere, permet d'arondire
+resultat4 = chiffre1 ** chiffre2  # exposant 
+resultat5 = chiffre1 % chiffre2   #modulo , reste d'une division entiere
+
+print(resultat5)
